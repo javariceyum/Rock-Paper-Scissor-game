a8b71b5052293b2c5e1becec585f4c46fd2f044e
javariceyum <javariceyum@gmail.com>
2023-10-23 18:02:17 +0800

    game function working but didnt use loop

--- script.js
+++ script.js
@@ -9,17 +9,42 @@ function getComputerChoice(){
 
 function playRounds(playerSelection, computerSelection) {
     if (playerSelection === "Rock" && computerSelection === "Scissor"){
-        return("You win");
+        return("You win! You cant cut the Rock");
     } else if(playerSelection === "Paper" && computerSelection === "Rock"){
-                return ("You Win");
+        return ("You Win! Wrap that rock!");
+    }else if(playerSelection === "Scissor" && computerSelection === "Paper"){
+        return ("You Win! Cut that Paper!");
     }else if (playerSelection === computerSelection){
         return("Tie");
     } else{ return("You lost");}
     }
 
-const playerSelection = "Rock" ;
-const computerSelection = getComputerChoice();
+function game(){
+    let playerSelection = prompt("Choose your weapon!");
+    let computerSelection = getComputerChoice();
+       console.log(playRounds(playerSelection, computerSelection));
+       console.log("")
+       
 
+       playerSelection = prompt("Choose your weapon!") ;
+       computerSelection = getComputerChoice();
+       console.log(playRounds(playerSelection, computerSelection));
+       console.log("")
 
-console.log(playRounds( playerSelection, computerSelection));
-    
+        playerSelection = prompt("Choose your weapon!") ;
+        computerSelection = getComputerChoice();
+       console.log(playRounds(playerSelection, computerSelection));
+       console.log("")
+
+        playerSelection = prompt("Choose your weapon!") ;
+        computerSelection = getComputerChoice();
+       console.log(playRounds(playerSelection, computerSelection));
+       console.log("")
+
+        playerSelection = prompt("Choose your weapon!") ;
+        computerSelection = getComputerChoice();
+       console.log(playRounds(playerSelection, computerSelection));
+       console.log("")
+}
+
+game();
