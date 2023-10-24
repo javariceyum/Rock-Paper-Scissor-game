a8c26a71698e4eb7b0bd1e3dc9670a084aed15af
javariceyum <javariceyum@gmail.com>
2023-10-24 10:45:04 +0800

    prompt input now is not case sensitive

--- script.js
+++ script.js
@@ -1,47 +1,47 @@
 function getComputerChoice(){
-    const weapons = ["Rock", "Paper","Scissor"];
+    const weapons = ["ROCK", "PAPER","SCISSOR"];
     const random = weapons[Math.floor(Math.random() * weapons.length)];
-    console.log(random);
+    console.log(random)
     return random;
 }
 
 
 
 function playRounds(playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Scissor"){
+    if (playerSelection === "ROCK" && computerSelection === "SCISSOR"){
         return("You win! You cant cut the Rock");
-    } else if(playerSelection === "Paper" && computerSelection === "Rock"){
+    } else if(playerSelection === "PAPER" && computerSelection === "ROCK"){
         return ("You Win! Wrap that rock!");
-    }else if(playerSelection === "Scissor" && computerSelection === "Paper"){
+    }else if(playerSelection === "SCISSOR" && computerSelection === "PAPER"){
         return ("You Win! Cut that Paper!");
     }else if (playerSelection === computerSelection){
         return("Tie");
     } else{ return("You lost");}
     }
 
-function game(){
-    let playerSelection = prompt("Choose your weapon!");
+function game(){;
+    let playerSelection = prompt("Choose your weapon!").toUpperCase();
     let computerSelection = getComputerChoice();
        console.log(playRounds(playerSelection, computerSelection));
        console.log("")
        
 
-       playerSelection = prompt("Choose your weapon!") ;
+       playerSelection = prompt("Choose your weapon!").toUpperCase();
        computerSelection = getComputerChoice();
        console.log(playRounds(playerSelection, computerSelection));
        console.log("")
 
-        playerSelection = prompt("Choose your weapon!") ;
+       playerSelection = prompt("Choose your weapon!").toUpperCase();
         computerSelection = getComputerChoice();
        console.log(playRounds(playerSelection, computerSelection));
        console.log("")
 
-        playerSelection = prompt("Choose your weapon!") ;
+       playerSelection = prompt("Choose your weapon!").toUpperCase();
         computerSelection = getComputerChoice();
        console.log(playRounds(playerSelection, computerSelection));
        console.log("")
 
-        playerSelection = prompt("Choose your weapon!") ;
+       playerSelection = prompt("Choose your weapon!").toUpperCase();
         computerSelection = getComputerChoice();
        console.log(playRounds(playerSelection, computerSelection));
        console.log("")
